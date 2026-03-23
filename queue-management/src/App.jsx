e4e2d232fa4e3c7152fa85d6e597ca90120b748b
Israel Gonzales <gonzalesisraeljoseph@gmail.com>
2026-03-24 07:55:52 +0800

    setup

---
+++ queue-management/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+export default function App(){
+  return(
+    <div>
+      <Header>
+        <h1>Queue Management Application</h1>
+        <p>Manage your customer efficienty</p>
+      </Header>
+    </div>
+  )
+}
